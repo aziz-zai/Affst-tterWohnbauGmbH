ced36dadb6d6bd4286c2df55f5eb97557b7db79c
Aziz <80921378+aziz-zai@users.noreply.github.com>
2022-12-29 12:20:02 +0100

    navbar design update

--- frontend_react/src/components/Home/Home.js
+++ frontend_react/src/components/Home/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from '../../assets/AWB-Logo.png'
 import './Home.scss'
 
 const Home = () => {
-  return <div className="app__hero-section">Home</div>
+  return (
+    <div className="app__hero-section">
+      <div className="app_hero-text">
+        <p>
+          Irure velit amet ad id ex deserunt magna proident cillum sint. Culpa excepteur proident
+          deserunt nulla. Mollit aliqua minim reprehenderit aliquip anim dolore nostrud anim velit
+          mollit pariatur qui. Nulla ullamco sunt do amet ex eu.
+        </p>
+        <button></button>
+      </div>
+      <img src={logo} alt="logo" />
+    </div>
+  )
 }
 
 export default Home
