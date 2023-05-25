752b1a7cdc74d1cb3ddb92b13c7eeb3260133678
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-05-25 14:17:46 +0200

    fg

--- frontend_react/src/components/Offers/MoreDialog.jsx
+++ frontend_react/src/components/Offers/MoreDialog.jsx
@@ -1,47 +1,33 @@
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import CloseIcon from '@mui/icons-material/Close';
-import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
-import Slide from '@mui/material/Slide';
-import React, { useState } from 'react';
-import './Offers.scss';
+import { ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon, Close as CloseIcon } from '@mui/icons-material';
+import { Dialog, DialogContent, DialogTitle, IconButton, Slide } from '@mui/material';
+import React from 'react';
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+const  MoreDialog = ({ offerTitle, offerText, imgArray, open, handleClose }) => {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
 
+  const handlePrev = () => {
+    setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : imgArray.length - 1));
+  };
 
-function MoreDialog({offerTitle, offerText, imgArray, open, handleClose}) {
-    const handleDialogClose = () => {
-        handleClose()
-    }
-    const [index, setIndex] = useState(0)  
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex < imgArray.length - 1 ? prevIndex + 1 : 0));
+  };
+  const handleDialogClose = () => {
+    handleClose()
+}
 
-    function nextStep() {
-      if (index === imgArray.length - 1) {
-        setIndex(0)
-        return
-      }
-      setIndex(index + 1)
-    }
-  
-    function prevStep() {
-      if (index === 0) {
-        setIndex(imgArray.length - 1)
-        return
-      }
-      setIndex(index - 1)
-    }
-    const Transition = React.forwardRef(function Transition(props, ref) {
-      return <Slide direction="up" ref={ref} {...props} />;
-    });
   return (
-    open?
-    <Dialog open={open} fullScreen TransitionComponent={Transition}>
-        <DialogTitle sx={{color: "white", backgroundColor: "#FAC189"}}>{offerTitle} | {offerText}
+    <Dialog open={open} onClose={handleClose} fullScreen TransitionComponent={Transition}>
+       <DialogTitle sx={{color: "white", backgroundColor: "#FAC189", ml:"2rem", mr:"2rem", borderBottomRightRadius:"16px", borderBottomLeftRadius:"16px"}}>{offerTitle} | {offerText}
             <IconButton
                 aria-label="close"
                 onClick={handleDialogClose}
                 sx={{
                   position: "absolute",
-                  right: 7,
+                  right: "3rem",
                   top: 3,
                   color:"white", 
                 }}
@@ -49,34 +35,25 @@ function MoreDialog({offerTitle, offerText, imgArray, open, handleClose}) {
                 <CloseIcon sx={{fontSize:"2.6rem"}}/>
             </IconButton>
         </DialogTitle>
-        <DialogContent sx={{width: "1000px"}}>
-          <div className="gallery_items-wrapper">
-          <div className="gallery_items" style={{ transform: `translate(-${index * 100/imgArray.length}%)`}}>
-          {imgArray.map(img => 
-          <div style={{padding:"1rem"}}>
-            <img key={img} src={img} alt={`Bilder von ${offerTitle}`}/>
-          </div>
-          )}
-          </div>
-          </div>
-          <div className='gallery-nav'>
-          <IconButton className='prevButton navButtons' onClick={prevStep}  >
-        <ArrowBackIosIcon/>
-      </IconButton>
-            <div className='gallery-navdots'>
+      <DialogContent sx={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <div className='navigation'>
+          <IconButton onClick={handlePrev}>
+            <ChevronLeftIcon fontSize="large"/>
+          </IconButton>
+          <img src={imgArray[currentIndex]} alt={offerTitle} style={{ width: '100%', maxHeight: '80vh', objectFit: 'contain' }} />
+          <IconButton onClick={handleNext}>
+            <ChevronRightIcon fontSize="large"/>
+          </IconButton>
+        </div>
+        <div className='gallery-navdots'>
               {imgArray.map(item => <button 
-              onClick={() => setIndex(imgArray.indexOf(item))} key={item} 
+              onClick={() => setCurrentIndex(imgArray.indexOf(item))} key={item} 
               className='gallery-navdot-items' 
-              style={{backgroundColor: imgArray.indexOf(item) === index ? "#FAC189" : "#cbcbcb"}} />)}
+              style={{backgroundColor: imgArray.indexOf(item) === currentIndex ? "#FAC189" : "#cbcbcb"}} />)}
             </div>
-      <IconButton className='nextButton navButtons' onClick={nextStep} >
-        <ArrowForwardIosIcon/>
-      </IconButton>
-          </div>
-        </DialogContent>
+      </DialogContent>
     </Dialog>
-    :null
-  )
-}
+  );
+};
 
 export default MoreDialog
