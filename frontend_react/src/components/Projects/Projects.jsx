128e9effb58cd2d1d7ac7f95bad05cb459b8ea3d
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-09-04 19:56:24 +0200

    ifram map added

--- frontend_react/src/components/Projects/Projects.jsx
+++ frontend_react/src/components/Projects/Projects.jsx
@@ -1,38 +1,74 @@
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { IconButton } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import Elektro from '../../assets/elektro.png';
-import ACAR from '../../assets/partner/p_acar.jpg';
-import ADIL from '../../assets/partner/p_adil.jpg';
-import Bucher from '../../assets/partner/p_bucher.jpg';
-import Dittus from '../../assets/partner/p_dittus.jpg';
-import Eberhardt from '../../assets/partner/p_eberhardt.jpg';
-import Feld from '../../assets/partner/p_feld.jpg';
-import Haser from '../../assets/partner/p_haser.jpg';
-import Nestle from '../../assets/partner/p_nestle.jpg';
-import Scherer from '../../assets/partner/p_scherer.jpg';
-import Straub from '../../assets/partner/p_straub.jpg';
-import Unal from '../../assets/partner/p_unal.jpg';
-import Aff_Mehrfam from '../../assets/projects/Aff_Mehrfam.jpg';
-import Jettingen_Doppelhaus from '../../assets/projects/Jettingen_Doppelhaus.JPG';
-import Magstadt_Doppelhaus from '../../assets/projects/Magstadt_Doppelhaus.jpg';
-import Mönchberg_Reihenhaus from '../../assets/projects/Mönchberg_Reihenhaus.jpg';
-import Rottenburg_Doppelhaus from '../../assets/projects/Rottenburg_Doppelhaus.JPG';
-import AppWrap from '../../wrapper/AppWrap';
-import './Projects.scss';
-import SingleProject from './SingleProject';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import {IconButton} from '@mui/material'
+import React, {useEffect, useState} from 'react'
+import {useSwipeable} from 'react-swipeable'
+import Elektro from '../../assets/elektro.png'
+import ACAR from '../../assets/partner/p_acar.jpg'
+import ADIL from '../../assets/partner/p_adil.jpg'
+import Bucher from '../../assets/partner/p_bucher.jpg'
+import Dittus from '../../assets/partner/p_dittus.jpg'
+import Eberhardt from '../../assets/partner/p_eberhardt.jpg'
+import Feld from '../../assets/partner/p_feld.jpg'
+import Haser from '../../assets/partner/p_haser.jpg'
+import Nestle from '../../assets/partner/p_nestle.jpg'
+import Scherer from '../../assets/partner/p_scherer.jpg'
+import Straub from '../../assets/partner/p_straub.jpg'
+import Unal from '../../assets/partner/p_unal.jpg'
+import Aff_Mehrfam from '../../assets/projects/Aff_Mehrfam.jpg'
+import Jettingen_Doppelhaus from '../../assets/projects/Jettingen_Doppelhaus.JPG'
+import Magstadt_Doppelhaus from '../../assets/projects/Magstadt_Doppelhaus.jpg'
+import Mönchberg_Reihenhaus from '../../assets/projects/Mönchberg_Reihenhaus.jpg'
+import Rottenburg_Doppelhaus from '../../assets/projects/Rottenburg_Doppelhaus.JPG'
+import AppWrap from '../../wrapper/AppWrap'
+import './Projects.scss'
+import SingleProject from './SingleProject'
 
-const fakeData =[{key: 0, lightMode: false, backgroundColor:"#3F88C5", img:Aff_Mehrfam, headText: "Mehrfamilienhaus in Affstätt", pText:"Ein schönes Mehrfamilienhaus in Affstätt mit schönen Blick und netten Nachbarn"},
-{key: 1,lightMode: false, backgroundColor:"#D72638", img:Jettingen_Doppelhaus,headText: "Doppelhaus in Jettingen", pText:"Ein schönes Doppelhaus in Jettingen mit schönen Blick und netten Nachbarn"},
-{key: 2,lightMode: false, backgroundColor:"#F49D37", img:Magstadt_Doppelhaus,headText: "Doppelhaus in Magstadt", pText:"Ein schönes Doppelhaus in Magstadt mit schönen Blick und netten Nachbarn"},
-{key: 3,lightMode: false, backgroundColor:"#140F2D", img:Mönchberg_Reihenhaus,headText: "Drei Reihenhäuser in Mönchberg", pText:"Drei Reihenhäuser in Mönchberg mit schönen Blick und netten Nachbarn"},
-{key: 4,lightMode: false, backgroundColor:"#F22B29", img:Rottenburg_Doppelhaus,headText: "Doppelhaus in Rottenburg", pText:"Ein schönes Doppelhaus in Rottenburg mit schönen Blick und netten Nachbarn"},
+const fakeData = [
+  {
+    key: 0,
+    lightMode: false,
+    backgroundColor: '#3F88C5',
+    img: Aff_Mehrfam,
+    headText: 'Mehrfamilienhaus in Affstätt',
+    pText: 'Ein schönes Mehrfamilienhaus in Affstätt mit schönen Blick und netten Nachbarn',
+  },
+  {
+    key: 1,
+    lightMode: false,
+    backgroundColor: '#D72638',
+    img: Jettingen_Doppelhaus,
+    headText: 'Doppelhaus in Jettingen',
+    pText: 'Ein schönes Doppelhaus in Jettingen mit schönen Blick und netten Nachbarn',
+  },
+  {
+    key: 2,
+    lightMode: false,
+    backgroundColor: '#F49D37',
+    img: Magstadt_Doppelhaus,
+    headText: 'Doppelhaus in Magstadt',
+    pText: 'Ein schönes Doppelhaus in Magstadt mit schönen Blick und netten Nachbarn',
+  },
+  {
+    key: 3,
+    lightMode: false,
+    backgroundColor: '#140F2D',
+    img: Mönchberg_Reihenhaus,
+    headText: 'Drei Reihenhäuser in Mönchberg',
+    pText: 'Drei Reihenhäuser in Mönchberg mit schönen Blick und netten Nachbarn',
+  },
+  {
+    key: 4,
+    lightMode: false,
+    backgroundColor: '#F22B29',
+    img: Rottenburg_Doppelhaus,
+    headText: 'Doppelhaus in Rottenburg',
+    pText: 'Ein schönes Doppelhaus in Rottenburg mit schönen Blick und netten Nachbarn',
+  },
 ]
 
 function Projects() {
-  const [index, setIndex] = useState(0)  
+  const [index, setIndex] = useState(0)
 
   function nextStep() {
     if (index === fakeData.length - 1) {
@@ -57,52 +93,71 @@ function Projects() {
       // Swipe right, go to the previous image
       prevStep()
     }
-  };
+  }
   const handlers = useSwipeable({
     onSwipedLeft: () => handleSwipe('left'),
     onSwipedRight: () => handleSwipe('right'),
-  });
+  })
   return (
-    <div className='banner_container'>
-      <div {...handlers} className='project_wrapper' style={{ transform: `translate(-${index * 100/fakeData.length}%)`
-     }}>
-      {fakeData.map(project => 
-      <SingleProject key={project.key}lightMode={project.lightMode} 
-      backgroundColor={project.backgroundColor} 
-      img={project.img}
-      headText={project.headText} 
-      pText={project.pText}
-      fakeData={fakeData}
-      index={index}
-      />
-      )}
-       
+    <div className="banner_container">
+      <div
+        {...handlers}
+        className="project_wrapper"
+        style={{transform: `translate(-${(index * 100) / fakeData.length}%)`}}
+      >
+        {fakeData.map((project) => (
+          <SingleProject
+            key={project.key}
+            lightMode={project.lightMode}
+            backgroundColor={project.backgroundColor}
+            img={project.img}
+            headText={project.headText}
+            pText={project.pText}
+            fakeData={fakeData}
+            index={index}
+          />
+        ))}
       </div>
       <div className="activeNavOption_container">
         <div className="activeNavOption_wrapper">
-        {fakeData.map(data => 
-        data.key == index ?
-        <div key={data.key} className="activeNavOption" style={{backgroundColor:"white"}}/>
-        : <div key={data.key} className="activeNavOption"/>
-        )}
+          {fakeData.map((data) =>
+            data.key == index ? (
+              <div key={data.key} className="activeNavOption" style={{backgroundColor: 'white'}} />
+            ) : (
+              <div key={data.key} className="activeNavOption" />
+            )
+          )}
+        </div>
       </div>
-      </div>
-      <IconButton className='prevButton navButtons' onClick={prevStep} 
-      style={{color:  fakeData[index].lightMode ? "gray" : "white", borderColor:  fakeData[index].lightMode ? "gray" : "white"}} >
-        <ArrowBackIosIcon/>
+      <IconButton
+        className="prevButton navButtons"
+        onClick={prevStep}
+        style={{
+          color: fakeData[index].lightMode ? 'gray' : 'white',
+          borderColor: fakeData[index].lightMode ? 'gray' : 'white',
+        }}
+      >
+        <ArrowBackIosIcon />
       </IconButton>
-      <IconButton className='nextButton navButtons' onClick={nextStep} 
-      style={{color:  fakeData[index].lightMode ? "gray" : "white", borderColor:  fakeData[index].lightMode ? "gray" : "white"}}>
-        <ArrowForwardIosIcon/>
+      <IconButton
+        className="nextButton navButtons"
+        onClick={nextStep}
+        style={{
+          color: fakeData[index].lightMode ? 'gray' : 'white',
+          borderColor: fakeData[index].lightMode ? 'gray' : 'white',
+        }}
+      >
+        <ArrowForwardIosIcon />
       </IconButton>
       <div className="partner_section">
         <h2>Besuchen Sie auch unsere Partner!</h2>
         <div>
-          <img src={Elektro} alt="elektro yer logo" className="elektro"/>
-          {[ACAR, ADIL, Bucher, Dittus, Eberhardt, Feld, Haser, Nestle, Scherer, Straub, Unal].map(partner => 
-            <img src={partner} alt={`${partner} logo`} className="partner"/>
-            )}
-    
+          <img src={Elektro} alt="elektro yer logo" className="elektro" />
+          {[ACAR, ADIL, Bucher, Dittus, Eberhardt, Feld, Haser, Nestle, Scherer, Straub, Unal].map(
+            (partner) => (
+              <img src={partner} alt={`${partner} logo`} className="partner" />
+            )
+          )}
         </div>
       </div>
     </div>
--- frontend_react/src/components/UeberUns/UeberUns.jsx
+++ frontend_react/src/components/UeberUns/UeberUns.jsx
@@ -6,34 +6,45 @@ import './UeberUns.scss'
 
 function UeberUns() {
   return (
-      <div className="uberuns_container">
-      <div className="uberuns_title">Über uns!</div>
-    <div className="uberuns_section">
-      <div className="map"><img src={map} alt="Standort Map"/></div>
-      <div className="uberuns_right_side">
-        <div>
-          <img src={avatar1} alt="Bild von Bilal"/>
-          <h3>Bilal Yer</h3>
-          <p>CEO & Founder</p>
+    <div className="uberuns_container">
+      <div className="uberuns_title">Über uns</div>
+      <div className="uberuns_section">
+        <div className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2638.890050816746!2d8.844331892067332!3d48.59280404536046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479759117ff3faaf%3A0xd557943efc01e569!2sTorgauer%20Weg%2015%2C%2071083%20Herrenberg!5e0!3m2!1sde!2sde!4v1693849578469!5m2!1sde!2sde"
+            width="100%"
+            allowFullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
-        <div>
-          <img src={avatar1} alt="Bild von Karl"/>
-          <h3>Karl Heinz</h3>
-          <p>Architekt & Visionär</p>
+        <div className="uberuns_right_side">
+          <div>
+            <img src={avatar1} alt="Bild von Bilal" />
+            <h3>Bilal Yer</h3>
+            <p>CEO & Founder</p>
+          </div>
+          <div>
+            <img src={avatar1} alt="Bild von Karl" />
+            <h3>Karl Heinz</h3>
+            <p>Architekt & Visionär</p>
+          </div>
         </div>
-      </div>
-      <div className="uberuns_text">
-        <h2>Wer sind wir?</h2>
-        <p>
-        Durch eine Vielzahl an Erfahrungen mit Bauherren über die Jahre hinweg, kam der Wunsch auf, das eigene Wissen und die langjährige Erfahrung im Baugewerbe in eine eigene Firma zu verwandeln.
-So wurde im Jahr 2000 bereits der Grundstein für die "Affstätter Wohnbau GmbH" gelegt. 
-Die bereits vorhandene Zusammenarbeit mit lokalen und überregionalen Maklern und Banken ermöglicht uns die Suche nach Grundstücken jeglicher Art und Größe vom Start weg für unsere Kunden zu erleichtern.
-Die einzelnen Bauabschnitte begleiten wir mit Ihnen gemeinsam im ständigen Dialog, sowohl auf der Baustelle als auch aus der Ferne.
-Herzlich willkommen in Ihrem Zuhause!
-        </p>
+        <div className="uberuns_text">
+          <h2>Wer sind wir?</h2>
+          <p>
+            Durch eine Vielzahl an Erfahrungen mit Bauherren über die Jahre hinweg, kam der Wunsch
+            auf, das eigene Wissen und die langjährige Erfahrung im Baugewerbe in eine eigene Firma
+            zu verwandeln. So wurde im Jahr 2000 bereits der Grundstein für die "Affstätter Wohnbau
+            GmbH" gelegt. Die bereits vorhandene Zusammenarbeit mit lokalen und überregionalen
+            Maklern und Banken ermöglicht uns die Suche nach Grundstücken jeglicher Art und Größe
+            vom Start weg für unsere Kunden zu erleichtern. Die einzelnen Bauabschnitte begleiten
+            wir mit Ihnen gemeinsam im ständigen Dialog, sowohl auf der Baustelle als auch aus der
+            Ferne. Herzlich willkommen in Ihrem Zuhause!
+          </p>
+        </div>
       </div>
     </div>
-      </div>
   )
 }
 
